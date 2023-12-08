d382acc1ea825778cacd25103b96ccf1e67e122b
guitri-ada <guillaume.trionfo@ada.ac.uk>
2023-12-08 12:41:48 +0000

    make PlayerInputHandler static

--- app/src/main/kotlin/treasureisland/model/Player.kt
+++ app/src/main/kotlin/treasureisland/model/Player.kt
@@ -2,13 +2,21 @@ package treasureisland.model
 
 
 class Player(
+    val name: String,
     var score: Int = 0,
     private var leaderboard: MutableList<Int> = mutableListOf()
 ) {
 
-    // get leaderboard
-    val getLeaderboard: MutableList<Int>
-        get() = leaderboard
+
+    // print leaderboard
+    fun printLeaderboard() {
+        var position = 1
+        leaderboard.forEach() {
+                score ->
+            println("${position}. ${score}")
+            position += 1
+        }
+    }
 
     // add score to leaderboard
     fun addScoreToLeaderboard(gameScore: Int) {
